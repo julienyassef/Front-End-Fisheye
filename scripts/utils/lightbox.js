63e46ad8e19f834a6a5b5a20b312b5385670b578
julien yassef <juyassef@gmail.com>
2023-10-14 07:08:14 +0200

    dernière vérifif et commenter son code, prépa soutenance

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -1,15 +1,19 @@
 
 class Lightbox {
     static init() {
+        // Sélectionne tous les liens d'images/vidéos
         const links = Array.from(document.querySelectorAll('.media-photographer__a'))
+        // Crée une galerie d'images/vidéos avec les liens et titres
         const gallery = links.map(link => ({href: link.getAttribute('href'), title: link.getAttribute('title')}))
 
+        // Ajoute un gestionnaire d'événement à chaque lien pour ouvrir le Lightbox
         links.forEach(link => link.addEventListener('click', e => {
             e.preventDefault()
             new Lightbox(e.currentTarget.getAttribute('href'), gallery);
         }));
     }
-    // ouverture du lightbox, focus sur le premier élément
+
+    // Focus sur le premier élément lors de l'ouverture du Lightbox
     focusOnOpen() {
         if (!this.element.contains(document.activeElement)) {
             const firstFocusableElement = this.element.querySelector('.lightbox__close');
@@ -20,8 +24,10 @@ class Lightbox {
     }
     
     constructor(url, mediaOfPhotographer) {
+        // Aplatit la liste de médias du photographe
         const mediaOfPhotographerFlat = mediaOfPhotographer.map(media => media.href).flat();
         
+        // Crée la structure DOM du Lightbox
         const element = this.buildDOM(url, mediaOfPhotographer);
         this.mediaOfPhotographerFlat = mediaOfPhotographer;
         this.currentIndex = mediaOfPhotographerFlat.findIndex(media => media === url);
@@ -30,9 +36,11 @@ class Lightbox {
         document.body.appendChild(element);
         document.addEventListener('keydown', this.onKeyDown.bind(this));
 
+        // Met le focus sur le premier élément lors de l'ouverture
         this.focusOnOpen();
     }
-    
+
+    // Gère les événements clavier 
     onKeyDown(e) {
         switch (e.key) {
             case 'ArrowLeft':
@@ -49,6 +57,7 @@ class Lightbox {
         }
     }
 
+     // Ferme le Lightbox
     close(e){
         e.preventDefault()
         this.element.classList.add('fadeOut')
@@ -59,6 +68,7 @@ class Lightbox {
         
     }
 
+    // Affiche le média suivant
     next(e){
         e.preventDefault();
         this.currentIndex = (this.currentIndex + 1) % this.mediaOfPhotographerFlat.length;
@@ -66,6 +76,7 @@ class Lightbox {
         this.loadContent(nextURL) 
     }
     
+     // Affiche le média précédent
     prev(e){
         e.preventDefault()
         this.currentIndex = (this.currentIndex - 1 + this.mediaOfPhotographerFlat.length) % this.mediaOfPhotographerFlat.length;
@@ -73,13 +84,16 @@ class Lightbox {
         this.loadContent(prevURL); 
     }
 
+    // Charge un nouveau contenu dans le Lightbox
     loadContent(url) {
         // Supprimez l'ancien contenu du Lightbox
         this.element.innerHTML = '';
+        // Construit la structure DOM avec le nouveau média
         const content = this.buildDOM(url, this.mediaOfPhotographerFlat);
         this.element.appendChild(content);
     }
-    
+
+    // Construit la structure DOM du Lightbox avec le média spécifié
     buildDOM(url, mediaOfPhotographer) {
         const media = mediaOfPhotographer.find(media => media.href === url)
         const dom = document.createElement('div');
@@ -108,7 +122,7 @@ class Lightbox {
             </div>`;
         }
         
-
+        // Ajoute des gestionnaires d'événements aux boutons
         dom.querySelector('.lightbox__close').addEventListener('click', this.close.bind(this))
         dom.querySelector('.lightbox__next').addEventListener('click', this.next.bind(this))
         dom.querySelector('.lightbox__prev').addEventListener('click', this.prev.bind(this))
